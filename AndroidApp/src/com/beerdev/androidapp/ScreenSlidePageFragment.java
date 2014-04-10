d76579fa3c6c38dde6cee80ef1eb332da81fbda0
Joakim Larsson <joakimlarsson92@gmail.com>
2014-04-10 17:28:12 +0200

    Fixed a solution for swipe between 5 products, also started with the TextFragment

--- AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
@@ -1,15 +1,15 @@
 package com.beerdev.androidapp;
 
-import com.beerdev.androidapp.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.support.v4.app.Fragment;
 
 
 public class ScreenSlidePageFragment extends Fragment {
@@ -17,23 +17,39 @@ public class ScreenSlidePageFragment extends Fragment {
 
 	private ArrayList<HashMap<String, String>> prodFragList;
 	
+	int mCurrentPage;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        /** Getting the arguments to the Bundle object */
+        Bundle data = getArguments();
+ 
+        /** Getting integer data of the key current_page from the bundle */
+        mCurrentPage = data.getInt("current_page", 0);
+ 
+    }
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
-
+        
         prodFragList = (ArrayList<HashMap<String, String>>) ScreenSlidePagerActivity.getArrayList();
         
         //Loader image
         int loader = R.drawable.ic_launcher;
-        
+
         // Imageview to show
         ImageView image = (ImageView) rootView.findViewById(R.id.slideImageView);
-
+        
         // Image url
-        String image_url = prodFragList.get(0).get("URL");
-         
+        String image_url = prodFragList.get(mCurrentPage).get("URL");
+        Log.i("URL", image_url);
+        Log.i("POSITION", Integer.toString(mCurrentPage));
+        
         // ImageLoader class instance
         ImageLoader imgLoader = new ImageLoader(rootView.getContext());
         
--- AndroidApp/src/com/beerdev/androidapp/ScreenSlidePagerActivity.java
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePagerActivity.java
@@ -23,13 +23,14 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
      * The pager widget, which handles animation and allows swiping horizontally to access previous
      * and next wizard steps.
      */
-    private ViewPager mPager;
+    private static ViewPager mPager;
 
     /**
      * The arraylist including all the products.
      */
     private static ArrayList<HashMap<String, String>> prodList;
     
+    private static int currentPage;
     
     /**
      * The pager adapter, which provides the pages to the view pager widget.
@@ -75,45 +76,31 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
         return prodList;
     }
     
+    public static int getCurrentItem(){
+    	return mPager.getCurrentItem();
+    }
     /**
      * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
      * sequence.
      */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
         public ScreenSlidePagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return new ScreenSlidePageFragment();
+        	ScreenSlidePageFragment myFragment = new ScreenSlidePageFragment();
+            Bundle data = new Bundle();
+            data.putInt("current_page", position);
+            myFragment.setArguments(data);
+            return myFragment;
         }
 
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
+
     }
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-    // Raw height and width of image
-    final int height = options.outHeight;
-    final int width = options.outWidth;
-    int inSampleSize = 1;
-
-    if (height > reqHeight || width > reqWidth) {
-
-        final int halfHeight = height / 2;
-        final int halfWidth = width / 2;
-
-        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-        // height and width larger than the requested height and width.
-        while ((halfHeight / inSampleSize) > reqHeight
-                && (halfWidth / inSampleSize) > reqWidth) {
-            inSampleSize *= 2;
-        }
-    }
-
-    return inSampleSize;
-}
 }
