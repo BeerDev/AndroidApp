800ff00c1aa59ea52d518df4c7d210c9cc3fec0b
Joakim Larsson <joakimlarsson92@gmail.com>
2014-04-10 12:13:21 +0200

    Swipe works but only one beer is showing, and no background is set!

--- AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
@@ -1,20 +1,44 @@
 package com.beerdev.androidapp;
 
+import com.beerdev.androidapp.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.support.v4.app.Fragment;
 
 
 public class ScreenSlidePageFragment extends Fragment {
 
+
+	private ArrayList<HashMap<String, String>> prodFragList;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
 
+        prodFragList = (ArrayList<HashMap<String, String>>) ScreenSlidePagerActivity.getArrayList();
+        
+        //Loader image
+        int loader = R.drawable.ic_launcher;
+        
+        // Imageview to show
+        ImageView image = (ImageView) rootView.findViewById(R.id.slideImageView);
+
+        // Image url
+        String image_url = prodFragList.get(0).get("URL");
+         
+        // ImageLoader class instance
+        ImageLoader imgLoader = new ImageLoader(rootView.getContext());
+        
+        imgLoader.DisplayImage(image_url, loader, image);
+        
         return rootView;
     }
 }
--- AndroidApp/src/com/beerdev/androidapp/ScreenSlidePagerActivity.java
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePagerActivity.java
@@ -1,5 +1,13 @@
 package com.beerdev.androidapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -12,7 +20,7 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
     /**
      * The number of pages (wizard steps) to show in this demo.
      */
-    private static final int NUM_PAGES = 5;
+    private static int NUM_PAGES;
 
     /**
      * The pager widget, which handles animation and allows swiping horizontally to access previous
@@ -20,6 +28,12 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
      */
     private ViewPager mPager;
 
+    /**
+     * The arraylist including all the products.
+     */
+    private static ArrayList<HashMap<String, String>> prodList;
+    
+    
     /**
      * The pager adapter, which provides the pages to the view pager widget.
      */
@@ -29,11 +43,22 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_slide);
-
+        
+        Intent intent = getIntent();
+        
+        //Gets the arraylist including all the products
+        prodList =(ArrayList<HashMap<String,String>>) intent.getSerializableExtra("contactList");
+        
+        //Define the number of viewpages that shall be included in the scrollview
+        NUM_PAGES = prodList.size();
+        
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+        //Bitmap bitBackground = BitmapFactory.decodeResource(getResources(), R.drawable.testbild);
+        //Drawable d = new BitmapDrawable(getResources(),bitBackground); 
+        //this.getWindow().getDecorView().setBackground(d);
     }
 
     @Override
@@ -48,6 +73,14 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
         }
     }
 
+    /**
+     * The method returns an arraylist which includes product with its information.
+     * @return prodList
+     */
+    public final static ArrayList<HashMap<String, String>> getArrayList() {
+        return prodList;
+    }
+    
     /**
      * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
      * sequence.
