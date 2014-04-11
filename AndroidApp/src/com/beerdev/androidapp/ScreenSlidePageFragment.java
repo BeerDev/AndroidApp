b6db98e8bb047cab6eaafe672bb7e3bee835f49a
Joakim Larsson <joakimlarsson92@gmail.com>
2014-04-11 09:23:24 +0200

    Test

--- AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
