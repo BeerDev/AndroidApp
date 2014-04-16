32feec51dd630138d559a050da7f2e0471f8cb6c
Joakim Larsson <joakimlarsson92@gmail.com>
2014-04-16 16:41:29 +0200

    Changes to fit merging with Patrik, this is info from swipeView

---
+++ AndroidApp/src/com/beerdev/androidapp/ScreenSlidePageFragment.java
@@ -0,0 +1,125 @@
+package com.beerdev.androidapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class ScreenSlidePageFragment extends Fragment {
+
+
+	private ArrayList<HashMap<String, String>> prodFragList;
+
+	/**
+     * The argument key for the page number this fragment represents.
+     */
+    public static final String ARG_PAGE = "page";
+
+    /**
+     * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
+     */
+    private int mPageNumber;
+    
+
+    /**
+     * Factory method for this fragment class. Constructs a new fragment for the given page number.
+     */
+    public static ScreenSlidePageFragment create(int pageNumber) {
+        ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ScreenSlidePageFragment() {
+    }
+     
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNumber = getArguments().getInt(ARG_PAGE);
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.swipe_fragment_layout, container, false);
+        
+   
+        //FrameLayout myLayout = (FrameLayout) rootView.findViewById(R.id.text_frame_layout);
+        //final FrameLayout fl = (FrameLayout) rootView.findViewById(R.id.frame_layout_text);
+        
+        prodFragList = (ArrayList<HashMap<String, String>>) ScreenSlidePagerActivity.getArrayList();
+        
+        final LinearLayout relL = (LinearLayout) rootView.findViewById(R.id.text_layout);
+        
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.slideImageView);
+          
+        //Loader image
+        int loader = R.drawable.ic_launcher;
+        String image_url = prodFragList.get(mPageNumber).get("URL");
+        ImageLoader imgLoader = new ImageLoader(rootView.getContext());
+        imgLoader.DisplayImage(image_url, loader, imageView);
+        
+        
+        //-----TEXT!--------
+        //Getting TextView
+        TextView textBeerName = (TextView) rootView.findViewById(R.id.swipeBeerName);
+        TextView textBeerPrice = (TextView) rootView.findViewById(R.id.swipeBeerPrice);
+        TextView textBeerSize = (TextView) rootView.findViewById(R.id.swipeBeerSize);
+        TextView textBeerPercent = (TextView) rootView.findViewById(R.id.swipeBeerPercent);
+        
+        //Getting Objects from JSON
+        String nameText = prodFragList.get(mPageNumber).get("Artikelnamn");
+        String priceText = prodFragList.get(mPageNumber).get("Utpris exkl moms");
+        String sizeText = prodFragList.get(mPageNumber).get("Storlek");
+        String percentText = prodFragList.get(mPageNumber).get("Alkoholhalt");
+
+        //Setting TextViews
+        textBeerName.setText(nameText);
+        textBeerPrice.setText(priceText+"kr*");
+        textBeerSize.setText(sizeText+" ml");
+        textBeerPercent.setText(percentText);
+        
+        
+
+        
+        rootView.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+		        getChildFragmentManager()
+		        	.beginTransaction()
+		        	.setCustomAnimations(R.animator.slide_in_bottom, R.animator.slide_out_bottom)
+		        	.add(R.id.content, TextFragment.create(mPageNumber), "textFragment")
+		        	.addToBackStack(null)
+		        	.commit();
+	        		relL.setVisibility(View.INVISIBLE);
+			}
+        	
+        });
+        
+        return rootView;
+    }
+
+    /**
+     * Returns the page number represented by this fragment object.
+     */
+    public int getPageNumber() {
+        return mPageNumber;
+    }
+
+}
