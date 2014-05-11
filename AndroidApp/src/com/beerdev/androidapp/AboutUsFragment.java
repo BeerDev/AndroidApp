dbfc54420bc9124dc5583891e26fa2c9353572d5
Joakim Larsson <joakimlarsson92@gmail.com>
2014-05-11 17:43:28 +0200

    Facebook-likebutton in about us page

--- AndroidApp/src/com/beerdev/androidapp/AboutUsFragment.java
+++ AndroidApp/src/com/beerdev/androidapp/AboutUsFragment.java
@@ -1,11 +1,16 @@
 package com.beerdev.androidapp;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A view to show information about the developmentteam and the application
@@ -24,6 +29,28 @@ public class AboutUsFragment extends Fragment {
 		 
 		 //Remove searchcontainer
 		 getActivity().findViewById(R.id.search_container).setVisibility(View.INVISIBLE);
+		 
+		 ImageButton ibFacebook = (ImageButton) aboutusView.findViewById(R.id.ibFacebook);
+			
+		 ibFacebook.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent facebookIntent = getOpenFacebookIntent(getActivity());
+				startActivity(facebookIntent);
+			}
+			 
+		 });
+			
 		 return aboutusView;
+	}
+	public static Intent getOpenFacebookIntent(Context context) {
+
+		   try {
+		    context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+		    return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/229588483907154"));
+		   } catch (Exception e) {
+		    return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/beerdev"));
+		   }
 	}
 }
